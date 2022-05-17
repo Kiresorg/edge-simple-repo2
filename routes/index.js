cd5671a6ce3ba04209aafd5ea49316f43dc91de4
Erik Gross <erik.d.gross@gmail.com>
2022-05-17 12:23:38 -0400

    edited home screen message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'This Express app was initialized by Erik' });
 });
 
 module.exports = router;
